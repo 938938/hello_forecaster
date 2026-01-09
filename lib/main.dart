3c6411afc2378f6c98a5f560577b79c77e67a28d
938938 <938kjh@gmail.com>
2026-01-09 22:59:42 +0900

    [FEAT] 알림 패키지 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hello_forecaster/screens/home_screen.dart';
+import 'package:hello_forecaster/utils/notification.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService.init();
   await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
